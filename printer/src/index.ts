5d0dddb8cb9ccc98f6df85d09da2ec91b36c5b60
ttmx <git@tteles.dev>
2024-03-24 13:16:10 +0000

    Fancier logging and easier config

--- printer/src/index.ts
+++ printer/src/index.ts
@@ -4,12 +4,34 @@ import fs from 'fs';
 type StopId = string;
 type LineId = string;
 
+const API_URL = 'http://localhost:5051';
+const RENDER_URL = 'http://localhost:3000/schedule';
+const PARALLEL = 16;
+const PRINT_INTERVAL = 100;
+
 let count = 0;
+let total = 0;
 let start = process.hrtime();
+
+let pageGotoTime = 0;
+let pdfRenderTime = 0;
+let browserOpenTime = 0;
+
+function secondsToHms(d: number) {
+	d = Number(d);
+	let h = Math.floor(d / 3600);
+	let m = Math.floor(d % 3600 / 60);
+	let s = Math.floor(d % 3600 % 60);
+
+	let hDisplay = h > 0 ? h + (h == 1 ? ' hour, ' : ' hours, ') : '';
+	let mDisplay = m > 0 ? m + (m == 1 ? ' minute, ' : ' minutes, ') : '';
+	let sDisplay = s > 0 ? s + (s == 1 ? ' second' : ' seconds') : '';
+	return hDisplay + mDisplay + sDisplay;
+}
 
 async function fetchTimetables():Promise<[StopId, LineId][]> {
 	console.log('Fetching timetables...');
-	let response = await fetch('http://localhost:5051/timetables');
+	let response = await fetch(`${API_URL}/timetables`);
 	console.log('Timetables fetched successfully');
 	return response.json();
 }
@@ -19,16 +41,23 @@ async function saveTimetableAsPDF(page: Page, line_id: string, stop_id: string) 
 	let success = false;
 	while (!success) {
 		try {
-			// console.log(`Generating PDF for line_id: ${line_id}, stop_id: ${stop_id}`);
-			await page.goto(`http://localhost:3000/schedule/${line_id}/${stop_id}`, { waitUntil: 'load' });
+			let gotoStart = process.hrtime();
+			await page.goto(`${RENDER_URL}/${line_id}/${stop_id}`, { waitUntil: 'load' });
+			let gotoEnd = process.hrtime(gotoStart);
+			pageGotoTime += gotoEnd[0] + gotoEnd[1] / 1e9;
+
+			let renderStart = process.hrtime();
 			await page.pdf({ path: `pdfs/timetable-${line_id}-${stop_id}.pdf`, format: 'A4', printBackground: true });
+			let renderEnd = process.hrtime(renderStart);
+			pdfRenderTime += renderEnd[0] + renderEnd[1] / 1e9;
+
 			success = true;
 			count++;
-			if (count % 100 === 0) {
+			if (count % PRINT_INTERVAL === 0) {
 				const [seconds, nanoseconds] = process.hrtime(prevStart);
 				const totalTimeInSeconds = seconds + nanoseconds / 1e9;
-				const pdfsPerSecond = 100 / totalTimeInSeconds;
-				console.log(`Generated ${count} PDFs, Rate: ${pdfsPerSecond.toFixed(2)} PDFs/second`);
+				const pdfsPerSecond = PRINT_INTERVAL / totalTimeInSeconds;
+				console.log(`${count}/${total} @ ${pdfsPerSecond.toFixed(2)}/s`);
 				prevStart = process.hrtime();
 			}
 		} catch (e) {
@@ -45,14 +74,16 @@ async function processSegment(segment: [string, string][], page: Page) {
 
 async function parallelGen(PARALLEL: number, timetableIndex: [string, string][]) {
 	console.log('Starting parallel generation...');
-	const browser = await Puppeteer.launch();
+	let browserOpenStart = process.hrtime();
+	const browser = await Puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']	});
 	const pages = await Promise.all(Array.from({ length: PARALLEL }, () => browser.newPage()));
+	let browserOpenEnd = process.hrtime(browserOpenStart);
+	browserOpenTime += browserOpenEnd[0] + browserOpenEnd[1] / 1e9;
 
 	for (let page of pages) {
-		page.setJavaScriptEnabled(false);
+		await page.setJavaScriptEnabled(false);
 	}
 	start = process.hrtime();
-	await saveTimetableAsPDF(pages[0], '2711', '060011'); // Warm up the browser
 	const segments: [string, string][][] = [];
 	const segmentSize = Math.ceil(timetableIndex.length / PARALLEL);
 	for (let i = 0; i < PARALLEL; i++) {
@@ -64,19 +95,19 @@ async function parallelGen(PARALLEL: number, timetableIndex: [string, string][])
 	const [seconds, nanoseconds] = process.hrtime(start);
 	const totalTimeInSeconds = seconds + nanoseconds / 1e9;
 	const pdfsPerSecond = count / totalTimeInSeconds;
-	console.log(`Parallelism: ${PARALLEL}, PDFs generated: ${count}, Total time: ${totalTimeInSeconds.toFixed(2)} seconds, Rate: ${pdfsPerSecond.toFixed(2)} PDFs/second`);
+	console.log(`Rendered ${count} PDFs in ${secondsToHms(totalTimeInSeconds)} at ${pdfsPerSecond.toFixed(2)} PDFs/second`);
+	console.log(`Page.goto time: ${pageGotoTime.toFixed(2)} seconds, PDF render time: ${pdfRenderTime.toFixed(2)} seconds, Browser open time: ${browserOpenTime.toFixed(2)} seconds`);
 
 	count = 0; // Reset count for the next iteration
 	await browser.close();
 }
 
 async function main() {
-	console.log('Starting main function...');
-	const timetableIndex = (await fetchTimetables()).slice(0, 1000);
+	const timetableIndex = await fetchTimetables();
+	total = timetableIndex.length;
+	console.log(`Generating ${total} schedules with ${PARALLEL} workers...`);
 
-	const PARALLEL = 40;
 	await parallelGen(PARALLEL, timetableIndex);
-	console.log('Main function completed');
 }
 
 main();
