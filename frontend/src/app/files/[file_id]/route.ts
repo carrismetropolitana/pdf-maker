8ceeb99288d739ba87b73e0d6a262697bcc93cc9
ttmx <git@tteles.dev>
2024-09-12 17:45:08 +0000

    Fix file header when sending files

--- frontend/src/app/files/[file_id]/route.ts
+++ frontend/src/app/files/[file_id]/route.ts
@@ -1,24 +1,39 @@
 import fs from 'fs';
+import { stat } from 'fs/promises';
 import { NextRequest } from 'next/server';
-export async function GET(request: NextRequest,context:{params:{file_id:string}}) {
+import { basename } from 'path';
+export async function GET(request: NextRequest, context: { params: { file_id: string } }) {
   // check if ./files directory exists
   // else return 404
   const { file_id } = context.params;
 
 
+  const path = `./files/${file_id}`;
+  const stats = await stat(path)
   // check if file exists
-  if (!fs.existsSync(`./files/${file_id}`)) {
+  if (!fs.existsSync(path)) {
     return Response.json({ body: 'File not found' }, { status: 404 });
   }
-  const webPlatformStream = streamFile(`./files/${file_id}`)
-  return new Response(webPlatformStream)
+  const webPlatformStream = streamFile(path)
+  return new Response(webPlatformStream, {
+    status: 200,
+    headers: new Headers({
+      "content-disposition":
+        `attachment; filename=${basename(
+          path
+        )}`,
+      "content-type": "application/zip",
+      "content-length": stats.size + "",
+    }
+    )
+  });
 }
 
 
 async function* nodeStreamToIterator(stream: fs.ReadStream) {
-    for await (const chunk of stream) {
-        yield new Uint8Array(chunk);
-    }
+  for await (const chunk of stream) {
+    yield new Uint8Array(chunk);
+  }
 }
 function iteratorToStream(iterator: AsyncGenerator<Uint8Array>): ReadableStream {
   return new ReadableStream({
@@ -35,9 +50,9 @@ function iteratorToStream(iterator: AsyncGenerator<Uint8Array>): ReadableStream 
 function streamFile(path: string): ReadableStream {
   const nodeStream = fs.createReadStream(path);
   const data: ReadableStream = iteratorToStream(
-      nodeStreamToIterator(
-          nodeStream
-      )
+    nodeStreamToIterator(
+      nodeStream
+    )
   )
   return data
 }
